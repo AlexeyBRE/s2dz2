b18053df98e1c2925b3f36e998a843193142c6d1
Alexey <brech_36@mail.ru>
2023-01-21 12:19:56 +0900

    add prog

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//   что третьей цифры нет.
+Console.WriteLine("Введите трехзначное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n>1)
+{
+if(n<999 && n>99) 
+{
+    n=n%100;
+    Console.WriteLine($"{n=n%10}") ;
+    break;
+}
+else if(n>999){
+    n=n/10;
+    continue;
+}
+else if(n<100)
+{
+Console.WriteLine($"третьей цифры нет") ;
+break;
+}
+}
